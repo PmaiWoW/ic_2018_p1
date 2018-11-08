bf24575132cb526c9d7b837a53801fdd0bdb7d81
bolhas <serapinta90@hotmail.com>
2018-11-08 21:34:05 +0000

    Ficheiro ic.h

---
+++ ic2.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int amount = 0, number = 1;
+int balance = 100, currentbet = 0;
+char op;
+
+void nCase() {
+	printf(" - Choose a number between 1 to 35.\n  ");
+        scanf("%d", &number);
+}
